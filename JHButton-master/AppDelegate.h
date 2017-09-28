287f7cd975d5c31b53eea68a7200f83401cf8f7f
RayJiang <1184731421@qq.com>
2017-09-28 15:00:18 +0800

    Initial Commit

---
+++ JHButton-master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHButton-master
+//
+//  Created by RayJiang on 2017/9/28.
+//  Copyright © 2017年 rayjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHButton-master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHButton-master
+//
+//  Created by RayJiang on 2017/9/28.
+//  Copyright © 2017年 rayjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
